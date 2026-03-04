91eaa693b5be02c6d5a1e8c4185180c94f82821e
devanooo <trestantodevano@gmail.com>
2026-03-04 16:09:38 +0700

    ganti nama

---
+++ Pertemuan/Week 2/Hackerrank/musimkompetisi.cpp
@@ -0,0 +1,18 @@
+/* Mohon untuk Mengisi identitas berikut sebelum mengerjakan
+ Nama : Devano Trestanto
+ NIM  : 24060124140149
+ Lab  : D1
+*/
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+#define ll long long
+#define vt vector
+
+
+int main(){
+    
+}
